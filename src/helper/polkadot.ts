fcbeb288559a14552cead7ba62df23959e75c4d3
jasl <jasl9187@hotmail.com>
2022-01-10 16:36:59 +0800

    support rpc port

--- src/helper/polkadot.ts
+++ src/helper/polkadot.ts
@@ -1,12 +1,20 @@
 import {ApiPromise, WsProvider} from '@polkadot/api'
+import {HttpProvider} from'@polkadot/rpc-provider/http'
 import {khala} from '@phala/typedefs'
 import {memoize} from 'lodash'
 
 export const getApi = memoize(async () => {
-  const provider = new WsProvider(
-    process.env.ENDPOINT || 'wss://khala-archive.phala.network/ws',
-    1000
-  )
+  const endpoint = process.env.ENDPOINT || 'wss://khala-archive.phala.network/ws'
+  let provider = null;
+  if (endpoint.startsWith('wss://') || endpoint.startsWith('ws://')) {
+    provider = new WsProvider(endpoint, 1000)
+  } else if (endpoint.startsWith('https://') || endpoint.startsWith('http://')) {
+    provider = new HttpProvider(endpoint)
+  } else {
+    console.warn(`Invalid endpoint ${endpoint}`)
+    process.exit(1)
+  }
+
   const api = await ApiPromise.create({provider, types: khala})
   return api
 })
